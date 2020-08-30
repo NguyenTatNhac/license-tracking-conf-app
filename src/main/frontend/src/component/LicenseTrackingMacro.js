6608043f017c6a5740f6b578e77a464bd9711b6b
Nhac Tat Nguyen <nhac.tat.nguyen@mgm-tp.com>
2020-08-30 23:40:39 +0700

    MLT-33: Render macro in react table

--- src/main/frontend/src/component/LicenseTrackingMacro.js
+++ src/main/frontend/src/component/LicenseTrackingMacro.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import DynamicTable from '@atlaskit/dynamic-table';
 import './LicenseTrackingMacro.css';
 
@@ -57,96 +58,70 @@ function LicenseTrackingMacro() {
     ],
   };
 
-  const rows = [
-    {
-      cells: [
-        {
-          key: 'licenseId',
-          content: 'Demo license ID',
-        },
-        {
-          key: 'app',
-          content: 'Demo App',
-        },
-        {
-          key: 'company',
-          content: 'Demo Company GmbH',
-        },
-        {
-          key: 'contact',
-          content: 'Demo Contact Nguyen Tat Nhac',
-        },
-        {
-          key: 'licenseType',
-          content: 'Demo license Type Paid',
-        },
-        {
-          key: 'startDate',
-          content: '2020',
-        },
-        {
-          key: 'endDate',
-          content: '2021',
-        },
-        {
-          key: 'status',
-          content: 'Demo Active Status',
-        },
-      ],
-      key: 'firstRow',
-      onClick: () => console.log('Clicked on first row'),
-    },
-    {
-      cells: [
-        {
-          key: 'licenseId',
-          content: 'Demo license ID 2',
-        },
-        {
-          key: 'app',
-          content: 'Demo App 2',
-        },
-        {
-          key: 'company',
-          content: 'Demo Company GmbH 2',
-        },
-        {
-          key: 'contact',
-          content: 'Demo Contact Nguyen Tat Nhac 2',
-        },
-        {
-          key: 'licenseType',
-          content: 'Demo license Type Paid 2',
-        },
-        {
-          key: 'startDate',
-          content: '2020',
-        },
-        {
-          key: 'endDate',
-          content: '2025',
-        },
-        {
-          key: 'status',
-          content: 'Demo Active Status 2',
-        },
-      ],
-      key: 'secondRow',
-      onClick: () => console.log('Clicked on second row'),
-    },
-  ];
-
+  const [rows, setRows] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
-  setTimeout(() => {
-    setLoading(false);
-  }, 500);
+  useEffect(() => {
+    let url = AJS.params.baseUrl + '/rest/license-tracking/1/licenses';
+    axios.get(url)
+    .then(rowsResponse => {
+      let tableRows = rowsResponse.data.map(license => {
+          let contact = license.contact;
+          let technicalContact = contact.technicalContact;
+          return {
+            cells: [
+              {
+                key: 'licenseId',
+                content: license.licenseId,
+              },
+              {
+                key: 'app',
+                content: license.addonName,
+              },
+              {
+                key: 'company',
+                content: contact.company,
+              },
+              {
+                key: 'contact',
+                content: technicalContact.email,
+              },
+              {
+                key: 'licenseType',
+                content: license.licenseType,
+              },
+              {
+                key: 'startDate',
+                content: license.maintenanceStartDate,
+              },
+              {
+                key: 'endDate',
+                content: license.maintenanceEndDate,
+              },
+              {
+                key: 'status',
+                content: license.status,
+              },
+            ],
+            key: license.licenseId,
+          };
+        },
+      );
+
+      setRows(tableRows);
+    }).catch((error) => {
+      console.error('Error when fetch licenses', error);
+    })
+    .then(() => {
+      setLoading(false);
+    });
+  }, []); // Empty array to use this effect only one
 
   return (
     <DynamicTable
       head={head}
       rows={rows}
-      rowsPerPage={10}
+      rowsPerPage={20}
       loadingSpinnerSize="large"
       isLoading={isLoading}
       defaultSortKey="startDate"
