9116b1f207052ee10c163a12253e74e1f3936ae3
Nhac Tat Nguyen <nhac.tat.nguyen@mgm-tp.com>
2020-08-30 20:13:41 +0700

    Render React Hello World Macro

--- src/main/java/com/ntnguyen/app/confluence/licensetracking/macro/LicenseTrackingMacro.java
+++ src/main/java/com/ntnguyen/app/confluence/licensetracking/macro/LicenseTrackingMacro.java
@@ -7,12 +7,6 @@ import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.webresource.api.assembler.PageBuilderService;
-import com.ntnguyen.app.confluence.licensetracking.persistent.entity.LicenseEntity;
-import com.ntnguyen.app.confluence.licensetracking.service.LicenseService;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,33 +15,16 @@ import org.springframework.stereotype.Component;
 public class LicenseTrackingMacro implements Macro {
 
   private final PageBuilderService pageBuilderService;
-  private final LicenseService licenseService;
 
   @Autowired
-  public LicenseTrackingMacro(@ComponentImport PageBuilderService pageBuilderService,
-      LicenseService licenseService) {
+  public LicenseTrackingMacro(@ComponentImport PageBuilderService pageBuilderService) {
     this.pageBuilderService = pageBuilderService;
-    this.licenseService = licenseService;
   }
 
   @Override
   public String execute(Map<String, String> map, String s, ConversionContext conversionContext) {
-    Map<String, Object> ctx = new HashMap<>(map);
-    List<LicenseEntity> licenses = licenseService.getAll();
-    ctx.put("licenses", licenses);
-    ctx.put("action", this);
-    pageBuilderService.assembler().resources().requireWebResource(APP_KEY + ":common-resources");
     pageBuilderService.assembler().resources().requireWebResource(APP_KEY + ":mlt-macro-resource");
-    return getRenderedTemplate("templates/license-macro-view.vm", ctx);
-  }
-
-  public boolean isNotExpired(LicenseEntity license) {
-    return license.getMaintenanceEndDate().after(new Date());
-  }
-
-  public String formatDate(Date date) {
-    String pattern = "dd-MM-yyyy";
-    return new SimpleDateFormat(pattern).format(date);
+    return getRenderedTemplate("templates/license-macro-view.vm", map);
   }
 
   @Override
